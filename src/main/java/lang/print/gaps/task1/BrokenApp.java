7ec7155d4253f64ea41d37fdd82a498f0675b02c
Valerii Krutoshynskyi <valera9496@gmail.com>
2022-11-30 15:22:31 +0100

    Done 3.1/3

--- src/main/java/lang/print/gaps/task1/BrokenApp.java
+++ src/main/java/lang/print/gaps/task1/BrokenApp.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task1;
 public class BrokenApp{
     public static void main(String[] args) {
 
-        System.out.println("fix Me");
+        System.out.println("fixMe");
     }
 }
